c20e37132109ad31b85022c1cce6871eb9e156f5
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-06-12 22:16:56 +0100

    add basic cli command scaffold

---
+++ cmd/start.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// startCmd represents the start command
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "start your server",
+	Long:  `start your server`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("listening at ...")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(startCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
