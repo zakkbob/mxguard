e3073d303b83148c380148a5a5861776f6986e47
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-07-12 23:51:04 +0100

    Add dockertest for postgres

---
+++ db/postgresdockertest_test.go
@@ -0,0 +1,92 @@
+package db_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+	"context"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/zakkbob/mxguard/db"
+	"github.com/zakkbob/mxguard/internal/config"
+	_ "github.com/lib/pq"
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+	"github.com/rs/zerolog/log"
+)
+
+var conn db.Conn
+
+func TestMain(m *testing.M) {
+	// uses a sensible default on windows (tcp/http) and linux/osx (socket)
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatal().Msgf("Could not construct pool: %s", err)
+	}
+
+	err = pool.Client.Ping()
+	if err != nil {
+		log.Fatal().Msgf("Could not connect to Docker: %s", err)
+	}
+
+	// pulls an image, creates a container based on it and runs it
+	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "11",
+		Env: []string{
+			"POSTGRES_PASSWORD=secret",
+			"POSTGRES_USER=user_name",
+			"POSTGRES_DB=dbname",
+			"listen_addresses = '*'",
+		},
+	}, func(config *docker.HostConfig) {
+		// set AutoRemove to true so that stopped container goes away by itself
+		config.AutoRemove = true
+		config.RestartPolicy = docker.RestartPolicy{Name: "no"}
+	})
+	if err != nil {
+		log.Fatal().Msgf("Could not start resource: %s", err)
+	}
+
+	hostAndPort := resource.GetHostPort("5432/tcp")
+	databaseUrl := fmt.Sprintf("postgres://user_name:secret@%s/dbname?sslmode=disable", hostAndPort)
+
+	log.Info().Msgf("Connecting to database on url: %s", databaseUrl)
+
+	resource.Expire(120) // Tell docker to hard kill the container in 120 seconds
+
+	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
+	pool.MaxWait = 120 * time.Second
+	if err = pool.Retry(func() error {
+		conn, err = db.InitConn(&config.Config{
+			Postgres: config.PostgresConfig{
+				Url: hostAndPort,
+				DB: "dbname",
+				User: "user_name",
+				Password: "secret",
+				SSLmode: "disable",
+			},
+		})
+		if err != nil {
+			return err
+		}
+		return conn.Ping(context.Background())
+	}); err != nil {
+		log.Fatal().Msgf("Could not connect to docker: %s", err)
+	}
+
+	defer func() {
+		if err := pool.Purge(resource); err != nil {
+			log.Fatal().Msgf("Could not purge resource: %s", err)
+		}
+	}()
+
+	// run tests
+	m.Run()
+}
+
+// Is my dockertest thing even working?
+func TestDockerTest(t *testing.T) {
+	err := conn.Ping(context.Background())
+	assert.NoError(t, err, "should not error")
+}
