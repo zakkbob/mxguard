bfe01fecf8959029e0baf3ddbb5dfd91756ab26f
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-07-12 17:55:00 +0100

    Add helpers.GetStringFlagOrPrompt

--- cmd/helpers/flags.go
+++ cmd/helpers/flags.go
@@ -7,20 +7,59 @@ import (
 	"github.com/spf13/cobra"
 	"io"
 	"strings"
+	"strconv"
 )
 
+// Get string cobra flag if set, prompt user if missing
 func GetStringFlagOrPrompt(cmd *cobra.Command, reader io.Reader, name string, prompt string) (string, error) {
 	if !cmd.Flags().Changed(name) {
 		fmt.Print(prompt)
 		buf := bufio.NewReader(reader)
 		value, err := buf.ReadString('\n')
 		if err != nil {
-			return "", err
+			return "", fmt.Errorf("reading user input: %w", err)
 		}
 		return strings.TrimRight(value, "\r\n"), nil
 	}
 
 	return cmd.Flags().GetString(name)
+}
+
+// Get boolean cobra flag if set, prompt user if missing
+// Prompt: (Default is capitalised)
+// {name} (Y/n): 
+func GetBoolFlagOrPrompt(cmd *cobra.Command, reader io.Reader, name string) (bool, error) {
+	if cmd.Flags().Changed(name) {
+		return cmd.Flags().GetBool(name)
+	}
+
+	defValue, err := strconv.ParseBool(cmd.Flags().Lookup(name).DefValue)
+	if err != nil {
+		return false, fmt.Errorf("parsing bool argument's default value: %w", err) 
+	}
+	options := "y/N"
+	if defValue {
+		options = "Y/n"
+	}
+
+	for {
+		fmt.Printf("%s (%s): ", name, options)
+		buf := bufio.NewReader(reader)
+		value, err := buf.ReadString('\n')
+		if err != nil {
+			return false, fmt.Errorf("reading user input: %w", err)
+		}
+
+		value = strings.TrimRight(value, "\r\n")
+		
+		if value == "" {
+			return defValue, nil
+		} else if value == "n" || value == "N" {
+			return false, nil
+		} else if value == "y" || value == "Y" {
+			return true, nil
+		}
+	}
 }
 
 // func MustGetStringFlagOrPrompt(logger zerolog.Logger, cmd *cobra.Command, reader io.Reader, name string, prompt string) string {
--- cmd/helpers/flags_test.go
+++ cmd/helpers/flags_test.go
@@ -1,6 +1,7 @@
 package helpers_test
 
 import (
+	"fmt"
 	"bytes"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
@@ -14,9 +15,7 @@ func TestGetStringFlagOrPrompt_FlagPassed(t *testing.T) {
 	}
 
 	cmd.Flags().String("mockflag", "", "mock flag usage")
-
 	cmd.SetArgs([]string{"--mockflag=success"})
-
 	cmd.Execute()
 
 	input := bytes.NewBufferString("fail\n")
@@ -35,6 +34,9 @@ func TestGetStringFlagOrPrompt_FlagNotPassed(t *testing.T) {
 		Use: "mock",
 	}
 
+	cmd.Flags().String("mockflag", "", "mock flag usage")
+	cmd.Execute()
+
 	input := bytes.NewBufferString("success\n")
 
 	name := "mockflag"
@@ -42,6 +44,47 @@ func TestGetStringFlagOrPrompt_FlagNotPassed(t *testing.T) {
 
 	expected := "success"
 	got, err := helpers.GetStringFlagOrPrompt(cmd, input, name, prompt)
+
 	assert.NoError(t, err)
 	assert.Equal(t, expected, got)
+}
+
+func TestGetBoolFlagOrPrompt(t *testing.T) {
+	tests := []struct{
+		input string
+		flag string
+		defValue bool
+		expected bool
+	}{
+		{"n", "", false, false},
+		{"N", "", false, false},
+		{"y", "", false, true},
+		{"Y", "", false, true},
+		{"", "", false, false},
+		{"", "", true, true},
+		{"", "true", false, true},
+		{"", "false", false, false},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%s_%s_%t", tt.input, tt.flag, tt.defValue), func(t *testing.T) {
+			var cmd = &cobra.Command{
+				Use: "mock",
+			}
+			name := "mockflag"
+
+			cmd.Flags().Bool(name, tt.defValue, "mock flag usage")
+			if tt.flag != "" {
+				cmd.SetArgs([]string{fmt.Sprintf("--mockflag=%s", tt.flag)})
+			}
+			cmd.Execute()
+
+			input := bytes.NewBufferString(tt.input + string('\n'))
+
+			got, err := helpers.GetBoolFlagOrPrompt(cmd, input, name)
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.expected, got)
+		})
+	}
 }
